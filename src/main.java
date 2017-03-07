4044aba4b17b70f602f93a80bb78d78d7a271015
Yuri <creator.and.dev@gmail.com>
2017-03-07 14:22:16 +0200

    refactor main class (Main)

--- src/main.java
+++ src/main.java
@@ -4,7 +4,7 @@ import java.net.Socket;
 import java.net.SocketAddress;
 import java.util.ArrayList;
 
-public class main {
+public class Main {
 	public static void main(String[] args) throws IOException {
 		try {
 			//задаем целочисленной переменной имя порта
